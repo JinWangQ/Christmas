1521a0f39041ffb11981d64bb5d6298e07fe64d6
Jin <wangatqueens@gmail.com>
2018-01-06 22:14:25 -0500

    sceneChanging_js

--- christmas.js
+++ christmas.js
@@ -1,7 +1,48 @@
+function changePage(element, effect, callback) {
+	element
+	.addClass(effect)
+	.one("animationend webkitAnimationEnd", function() {
+		callback && callback();
+	})
+}
+
 var Christmas = function() {
 	var $pageA = $('.page-a');
-	new pageA($pageA);
+	var $pageB = $('.page-b');
+	
+	/*Test for the first scene */
+	// new pageA($pageA, function() {
+	// 	setTimeout(function() {
+	// 		alert('pageA finished')
+	// 	},20000)
+	// })
+
+	/* Test for the second scene */
+	// new pageB($pageB, function() {
+	// 	setTimeout(function() {
+	// 		alert("pageB finished")
+	// 	},20000)
+	// })
+
+	var observer = new Observer();
+	new pageA($pageA, function() {
+		observer.publish("completeA");
+	});
+
+	observer.subscribe("completeA", function() {
+		changePage($pageA, "effect-out", function() {
+			observer.publish("pageB");
+		})
+	})
+
+	observer.subscribe("pageB", function() {
+		new pageB($pageB, function() {
+			observer.publish("completeB");
+		})
+	})
+
 }
+
 
 $(function() {
 	$("button").click(function() {
