e91f69e5d6c6da6a8f7dd33186cdf05aeb37fdcb
Jin <wangatqueens@gmail.com>
2018-01-08 23:57:13 -0500

    pageB_add_carousel

--- pageB.js
+++ pageB.js
@@ -2,12 +2,16 @@ function pageB(element, callback) {
 
 	var $boy = element.find(".christmas-boy");
 	var $girl = element.find(".girl");
+	var $carousel =element.find("#carousel")
 	var animationEnd = "animationend webkitAnimationEnd";
+
 	var boyAction = {
 		walk: function() {
 			var dfd = $.Deferred();
 			$boy.addClass('boy-walk');
-			$boy.transition({"right": "4.5rem"}, 4000, "linear", function(){
+			$boy.transition({
+				"right": "4.5rem"
+			}, 4000, "linear", function(){
 				dfd.resolve()
 			});
 			return dfd;
@@ -49,13 +53,14 @@ function pageB(element, callback) {
 				$girl.addClass("girl-throwBook");
 				dfd.resolve();
 			}, 500)
-
 			return dfd;
 		},
 		walk: function() {
 			var dfd = $.Deferred();
 			$girl.addClass("girl-walk");
-			$girl.transition({"left":"4.5rem"}, 4000, "linear", function() {
+			$girl.transition({
+				"left":"4.5rem"
+			}, 3000, "linear", function() {
 				dfd.resolve()
 			});
 			return dfd;
@@ -75,15 +80,21 @@ function pageB(element, callback) {
 		},
 		weepWalk: function(callback) {
 			$girl.addClass("girl-weep");
-			$girl.transition({"left": "7rem"}, 1000, "linear", function() {
+			$girl.transition({
+				"left": "7rem"
+			}, 1000, "linear", function() {
 				$girl.addClass("walk-stop").removeClass("girl-weep")
 				callback();
 			});
 		},
 		hug: function() {
 			$girl.addClass("girl-hug").addClass("walk-run")
+		},
+		reset: function() {
+			$girl.removeClass("girl-choose");
 		}
 	}
+
 
 	boyAction.walk()
 		.then(function() {
@@ -104,28 +115,90 @@ function pageB(element, callback) {
 			return boyAction.unwrapp();
 		})
 		.then(function() {
-
-			setTimeout(function() {
-				boyAction.strip(1)
-			}, 1000)
-			setTimeout(function() {
-				boyAction.strip(2) 
-			}, 2000)
-			setTimeout(function() {
-				boyAction.strip(3)
-			}, 3000)
-
-			setTimeout(function() {
-				boyAction.hug();
-			}, 4000)
+			return rotation3d();
 		})
 		.then(function() {
-			girlAction.choose(function() {
-				girlAction.weepWalk(function() {
-					girlAction.hug();
-				})
+
+			girlAction.weepWalk(function() {
+				girlAction.hug();
+				boyAction.hug();
+				(function() {
+					callback && callback()
+				}).defer(1000)
 			})
 		})
+
+   function rotation3d() {
+        var dfd = $.Deferred();
+        setTimeout(function() {
+        	createGift(dfd);
+        },500)
+        return dfd;
+	}
+
+    function createGift(dfd) {
+        var carousel = createCarousel();
+        var start = 1;
+        var end = carousel.numpics;
+        var play = function() {
+            //get giftd
+            carouselGift(start, carousel, function() {
+                ++start;
+                next();
+            });
+        };
+        //check 
+        var next = function() {
+            //3 times
+            if (start > end) {
+                carousel.destroy();
+                setTimeout(function() {
+                	dfd.resolve();
+                },1000)
+                return
+            }
+            setTimeout(function() {
+            	play();
+            },1000)
+        };
+        play();
+	}
+
+    /**
+     * get gift
+     * 
+     */
+    function carouselGift(count, carousel, complete) {
+        //run 3 times
+        carousel.run(count);
+        //choose action
+        girlAction.choose(function() {
+            //select pic
+            carousel.selected(function() {
+                //girl's action reset
+                girlAction.reset();
+                boyAction.strip(count);
+
+                setTimeout(function() {
+                    complete();
+                }, 1000)
+            });
+        })
+    }
+
+
+    //创建3d旋转
+    function createCarousel() {
+        //3d旋转
+        var carousel = new Carousel($carousel, {
+            imgUrls: [
+                "http://img.mukewang.com/5662e26f00010dea14410901.png",
+                "http://img.mukewang.com/5662e2960001f16314410901.png",
+                "http://img.mukewang.com/5662e29a0001905a14410901.png"
+            ]
+        });
+        return carousel;
+    }
 
 
 	setTimeout(function() {
